dbf55a14442b2f9a16d33d611616f5e4ca1feb9d
Mark Tripney <mark.tripney@gmail.com>
2020-07-30 14:21:54 +0200

    Add placeholder 'Work' and 'Writing' pages.

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ year = date.today().year
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("index.html", title="marktripney | home")
+    return render_template("index.html", title="home")
+
+
+@app.route("/work")
+def work():
+    return render_template("work.html", title="work")
+
+
+@app.route("/writing")
+def writing():
+    return render_template("writing.html", title="writing")
 
 
 if __name__ == "__main__":
