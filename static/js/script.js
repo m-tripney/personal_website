51b6f9e139836e4f508c1016a1a3b7dcc2b9b3d4
Mark Tripney <mark.tripney@gmail.com>
2020-07-31 13:49:36 +0200

    Add 'copy to clipboard' option for email address.

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,21 @@
 // Populate footer with current year
 document.getElementById('year').innerHTML = new Date().getFullYear();
+// Copy to clipboard
+let flag = false;
+const clipped = new ClipboardJS('#email-addr');
+// const confirmationSpan = document.querySelector('.email-copied');
+const doneMessage = document.querySelector('.done-text');
+const completeEllipsis = document.querySelector('.add-ellipsis');
+clipped.on('success', (e) => {
+  const copyConfirm = document.querySelector('.copy-confirm');
+  if (flag === false) {
+    const ellipsis = '..';
+    const doneText = ' Done!';
+    // const confirmationText = ' I look forward to hearing from you!';
+    completeEllipsis.textContent += ellipsis;
+    doneMessage.textContent += doneText;
+    // confirmationSpan.textContent += confirmationText;
+  }
+  flag = true;
+  e.clearSelection();
+});
