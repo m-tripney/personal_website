adf1a274772914c2aacb6f65b947aeb080b7fefa
Mark Tripney <mark.tripney@gmail.com>
2020-08-03 17:24:59 +0200

    Clipboard script should only load on Contact page.

--- static/js/clip-script.js
+++ static/js/clip-script.js
@@ -1,9 +1,6 @@
-// Populate footer with current year
-document.getElementById('year').innerHTML = new Date().getFullYear();
 // Copy to clipboard
 let flag = false;
 const clipped = new ClipboardJS('#email-addr');
-// const confirmationSpan = document.querySelector('.email-copied');
 const doneMessage = document.querySelector('.done-text');
 const completeEllipsis = document.querySelector('.add-ellipsis');
 clipped.on('success', (e) => {
@@ -11,10 +8,8 @@ clipped.on('success', (e) => {
   if (flag === false) {
     const ellipsis = '..';
     const doneText = ' Done!';
-    // const confirmationText = ' I look forward to hearing from you!';
     completeEllipsis.textContent += ellipsis;
     doneMessage.textContent += doneText;
-    // confirmationSpan.textContent += confirmationText;
   }
   flag = true;
   e.clearSelection();
